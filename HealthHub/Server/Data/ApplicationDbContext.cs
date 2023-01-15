545173c17ee38a495af5c178cf50d02b15041e9f
Benjamin Keller <zhero240@gmail.com>
2023-01-15 21:54:12 +0200

    Add project files.

---
+++ HealthHub/Server/Data/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Duende.IdentityServer.EntityFramework.Options;
+using HealthHub.Server.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+
+namespace HealthHub.Server.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
---
+++ HealthHub/Shared/Client.cs
@@ -0,0 +1,12 @@
+﻿namespace HealthHub.Shared;
+
+public class Client
+{
+    public Guid ID { get; set; }
+    public Guid ClientId { get; set; }
+    public string FirstName { get; set; }
+    public string? SecondName { get; set; }
+    public string LastName { get; set; }
+    public DateOnly DateOfBirth { get; set; }
+    public string IDNumber { get; set; }
+}
