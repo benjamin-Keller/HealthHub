b7bfda6da3dd6e9d2acb29a5a1b033796efbb620
Benjamin Keller <zhero240@gmail.com>
2023-01-28 20:30:07 +0200

    Adds User model converters

---
+++ HealthHub/Server/Converters/UserConverters.cs
@@ -0,0 +1,21 @@
+﻿using HealthHub.Shared;
+
+namespace HealthHub.Server.Converters
+{
+    public static class UserConverters
+    {
+        public static ApplicationUser ToApplicationUser(this AddUserViewModel model)
+        {
+            return new ApplicationUser
+            {
+                FirstName = model.FirstName,
+                SecondName = model.SecondName ?? "",
+                LastName = model.LastName,
+                Email = model.Email,
+                IdNumber = model.IdNumber,
+                PhoneNumber = model.PhoneNumber,
+                UserName = model.Email,
+            };
+        }
+    }
+}
